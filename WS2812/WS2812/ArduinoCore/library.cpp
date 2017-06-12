2ec33bb41892302fef8f896b3602635770111003
Andreas Burnickl <a.burnickl@gmail.com>
2017-06-12 15:09:17 +0200

    - added WS2812 lib

---
+++ WS2812/WS2812/ArduinoCore/library.cpp
@@ -0,0 +1,16 @@
+/*
+ * ArduinoCore.cpp
+ *
+ * Created: 2017-04-12 13:34:36
+ * Author : virskl
+ */ 
+
+#include <avr/io.h>
+
+
+/* Replace with your library code */
+int myfunc(void)
+{
+	return 0;
+}
+
