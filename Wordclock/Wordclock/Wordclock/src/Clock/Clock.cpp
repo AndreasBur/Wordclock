d9ddbb73678b7654178e32065010d9ff56f84b5e
Andreas Burnickl <a.burnickl@gmail.com>
2017-07-06 09:20:30 +0200

    changed display words

--- Wordclock/Wordclock/Wordclock/src/Clock/Clock.cpp
+++ Wordclock/Wordclock/Wordclock/src/Clock/Clock.cpp
@@ -39,97 +39,97 @@
 ******************************************************************************************************************************************************/
 const DisplayWordsType Clock::ClockHoursTable[][CLOCK_NUMBER_OF_HOURS][CLOCK_MAX_NUMBER_OF_HOUR_WORDS] PROGMEM =
 {
-	{                                                               // ClockHoursTable[0][] = hh:00
-		{DISPLAY_WORD_ZWOELF,  DISPLAY_WORD_UHR },                  // 00:00
-		{DISPLAY_WORD_EIN,     DISPLAY_WORD_UHR },                  // 01:00
-		{DISPLAY_WORD_ZWEI,    DISPLAY_WORD_UHR },                  // 02:00
-		{DISPLAY_WORD_DREI_2,  DISPLAY_WORD_UHR },                  // 03:00
-		{DISPLAY_WORD_VIER_2,  DISPLAY_WORD_UHR },                  // 04:00
-		{DISPLAY_WORD_FUENF_2, DISPLAY_WORD_UHR },                  // 05:00
-		{DISPLAY_WORD_SECHS,   DISPLAY_WORD_UHR },                  // 06:00
-		{DISPLAY_WORD_SIEBEN,  DISPLAY_WORD_UHR },                  // 07:00
-		{DISPLAY_WORD_ACHT,    DISPLAY_WORD_UHR },                  // 08:00
-		{DISPLAY_WORD_NEUN,    DISPLAY_WORD_UHR },                  // 09:00
-		{DISPLAY_WORD_ZEHN_2,  DISPLAY_WORD_UHR },                  // 10:00
-		{DISPLAY_WORD_ELF,     DISPLAY_WORD_UHR }                   // 11:00
+	{                                                                   // ClockHoursTable[0][] = hh:00
+		{DISPLAY_WORD_HOUR_ZWOELF, DISPLAY_WORD_UHR },                  // 00:00
+		{DISPLAY_WORD_HOUR_EIN,    DISPLAY_WORD_UHR },                  // 01:00
+		{DISPLAY_WORD_HOUR_ZWEI,   DISPLAY_WORD_UHR },                  // 02:00
+		{DISPLAY_WORD_HOUR_DREI,   DISPLAY_WORD_UHR },                  // 03:00
+		{DISPLAY_WORD_HOUR_VIER,   DISPLAY_WORD_UHR },                  // 04:00
+		{DISPLAY_WORD_HOUR_FUENF,  DISPLAY_WORD_UHR },                  // 05:00
+		{DISPLAY_WORD_HOUR_SECHS,  DISPLAY_WORD_UHR },                  // 06:00
+		{DISPLAY_WORD_HOUR_SIEBEN, DISPLAY_WORD_UHR },                  // 07:00
+		{DISPLAY_WORD_HOUR_ACHT,   DISPLAY_WORD_UHR },                  // 08:00
+		{DISPLAY_WORD_HOUR_NEUN,   DISPLAY_WORD_UHR },                  // 09:00
+		{DISPLAY_WORD_HOUR_ZEHN,   DISPLAY_WORD_UHR },                  // 10:00
+		{DISPLAY_WORD_HOUR_ELF,    DISPLAY_WORD_UHR }                   // 11:00
 	},
-	{                                                               // ClockHoursTable[1][] = hh:mm
-		{DISPLAY_WORD_ZWOELF,  DISPLAY_WORD_NONE },                 // 00:mm
-		{DISPLAY_WORD_EINS,    DISPLAY_WORD_NONE },                 // 01:mm
-		{DISPLAY_WORD_ZWEI,    DISPLAY_WORD_NONE },                 // 02:mm
-		{DISPLAY_WORD_DREI_2,  DISPLAY_WORD_NONE },                 // 03:mm
-		{DISPLAY_WORD_VIER_2,  DISPLAY_WORD_NONE },                 // 04:mm
-		{DISPLAY_WORD_FUENF_2, DISPLAY_WORD_NONE },                 // 05:mm
-		{DISPLAY_WORD_SECHS,   DISPLAY_WORD_NONE },                 // 06:mm
-		{DISPLAY_WORD_SIEBEN,  DISPLAY_WORD_NONE },                 // 07:mm
-		{DISPLAY_WORD_ACHT,    DISPLAY_WORD_NONE },                 // 08:mm
-		{DISPLAY_WORD_NEUN,    DISPLAY_WORD_NONE },                 // 09:mm
-		{DISPLAY_WORD_ZEHN_2,  DISPLAY_WORD_NONE },                 // 10:mm
-		{DISPLAY_WORD_ELF,     DISPLAY_WORD_NONE }                  // 11:mm
+	{                                                                   // ClockHoursTable[1][] = hh:mm
+		{DISPLAY_WORD_HOUR_ZWOELF, DISPLAY_WORD_NONE },                 // 00:mm
+		{DISPLAY_WORD_HOUR_EINS,   DISPLAY_WORD_NONE },                 // 01:mm
+		{DISPLAY_WORD_HOUR_ZWEI,   DISPLAY_WORD_NONE },                 // 02:mm
+		{DISPLAY_WORD_HOUR_DREI,   DISPLAY_WORD_NONE },                 // 03:mm
+		{DISPLAY_WORD_HOUR_VIER,   DISPLAY_WORD_NONE },                 // 04:mm
+		{DISPLAY_WORD_HOUR_FUENF,  DISPLAY_WORD_NONE },                 // 05:mm
+		{DISPLAY_WORD_HOUR_SECHS,  DISPLAY_WORD_NONE },                 // 06:mm
+		{DISPLAY_WORD_HOUR_SIEBEN, DISPLAY_WORD_NONE },                 // 07:mm
+		{DISPLAY_WORD_HOUR_ACHT,   DISPLAY_WORD_NONE },                 // 08:mm
+		{DISPLAY_WORD_HOUR_NEUN,   DISPLAY_WORD_NONE },                 // 09:mm
+		{DISPLAY_WORD_HOUR_ZEHN,   DISPLAY_WORD_NONE },                 // 10:mm
+		{DISPLAY_WORD_HOUR_ELF,    DISPLAY_WORD_NONE }                  // 11:mm
 	}
 };
 
 
 const ClockMinutesType Clock::ClockMinutesTable[][CLOCK_NUMBER_OF_MINUTE_STEPS] PROGMEM =
 {
-	{                                                                                       // ClockMinutesTable[0][] = WESSI
-		{0},                                                                                // 00
-		{1, 0, {DISPLAY_WORD_FUENF_1,     DISPLAY_WORD_NACH, DISPLAY_WORD_NONE }},          // 05
-		{1, 0, {DISPLAY_WORD_ZEHN_1,      DISPLAY_WORD_NACH, DISPLAY_WORD_NONE }},          // 10
-		{1, 0, {DISPLAY_WORD_VIERTEL,     DISPLAY_WORD_NACH, DISPLAY_WORD_NONE }},          // 15
-		{1, 1, {DISPLAY_WORD_ZEHN_1,      DISPLAY_WORD_VOR,  DISPLAY_WORD_HALB }},          // 20
-		{1, 1, {DISPLAY_WORD_FUENF_1,     DISPLAY_WORD_VOR,  DISPLAY_WORD_HALB }},          // 25
-		{1, 1, {DISPLAY_WORD_HALB,        DISPLAY_WORD_NONE, DISPLAY_WORD_NONE }},          // 30
-		{1, 1, {DISPLAY_WORD_FUENF_1,     DISPLAY_WORD_NACH, DISPLAY_WORD_HALB }},          // 35
-		{1, 1, {DISPLAY_WORD_ZEHN_1,      DISPLAY_WORD_NACH, DISPLAY_WORD_HALB }},          // 40
-		{1, 1, {DISPLAY_WORD_VIERTEL,     DISPLAY_WORD_VOR,  DISPLAY_WORD_NONE }},          // 45
-		{1, 1, {DISPLAY_WORD_ZEHN_1,      DISPLAY_WORD_VOR,  DISPLAY_WORD_NONE }},          // 50
-		{1, 1, {DISPLAY_WORD_FUENF_1,     DISPLAY_WORD_VOR,  DISPLAY_WORD_NONE }},          // 55
+	{                                                                                         // ClockMinutesTable[0][] = WESSI
+		{0},                                                                                  // 00
+		{1, 0, {DISPLAY_WORD_FUENF,       DISPLAY_WORD_NACH, DISPLAY_WORD_NONE   }},          // 05
+		{1, 0, {DISPLAY_WORD_ZEHN,        DISPLAY_WORD_NACH, DISPLAY_WORD_NONE   }},          // 10
+		{1, 0, {DISPLAY_WORD_VIERTEL,     DISPLAY_WORD_NACH, DISPLAY_WORD_NONE   }},          // 15
+		{1, 1, {DISPLAY_WORD_ZEHN,        DISPLAY_WORD_VOR,  DISPLAY_WORD_HALB   }},          // 20
+		{1, 1, {DISPLAY_WORD_FUENF,       DISPLAY_WORD_VOR,  DISPLAY_WORD_HALB   }},          // 25
+		{1, 1, {DISPLAY_WORD_HALB,        DISPLAY_WORD_NONE, DISPLAY_WORD_NONE   }},          // 30
+		{1, 1, {DISPLAY_WORD_FUENF,       DISPLAY_WORD_NACH, DISPLAY_WORD_HALB   }},          // 35
+		{1, 1, {DISPLAY_WORD_ZEHN,        DISPLAY_WORD_NACH, DISPLAY_WORD_HALB   }},          // 40
+		{1, 1, {DISPLAY_WORD_VIERTEL,     DISPLAY_WORD_VOR,  DISPLAY_WORD_NONE   }},          // 45
+		{1, 1, {DISPLAY_WORD_ZEHN,        DISPLAY_WORD_VOR,  DISPLAY_WORD_NONE   }},          // 50
+		{1, 1, {DISPLAY_WORD_FUENF,       DISPLAY_WORD_VOR,  DISPLAY_WORD_NONE   }},          // 55
 	},
 
-	{                                                                                       // ClockMinutesTable[1][] = OSSI
-		{0},                                                                                // 00
-		{1, 0, {DISPLAY_WORD_FUENF_1,     DISPLAY_WORD_NACH, DISPLAY_WORD_NONE }},          // 05
-		{1, 0, {DISPLAY_WORD_ZEHN_1,      DISPLAY_WORD_NACH, DISPLAY_WORD_NONE }},          // 10
-		{1, 1, {DISPLAY_WORD_VIERTEL,     DISPLAY_WORD_NONE, DISPLAY_WORD_NONE }},          // 45
-		{1, 1, {DISPLAY_WORD_ZEHN_1,      DISPLAY_WORD_VOR,  DISPLAY_WORD_HALB }},          // 20
-		{1, 1, {DISPLAY_WORD_FUENF_1,     DISPLAY_WORD_VOR,  DISPLAY_WORD_HALB }},          // 25
-		{1, 1, {DISPLAY_WORD_HALB,        DISPLAY_WORD_NONE, DISPLAY_WORD_NONE }},          // 30
-		{1, 1, {DISPLAY_WORD_FUENF_1,     DISPLAY_WORD_NACH, DISPLAY_WORD_HALB }},          // 35
-		{1, 1, {DISPLAY_WORD_ZEHN_1,      DISPLAY_WORD_NACH, DISPLAY_WORD_HALB }},          // 40
-		{1, 1, {DISPLAY_WORD_DREIVIERTEL, DISPLAY_WORD_NONE, DISPLAY_WORD_NONE }},          // 15
-		{1, 1, {DISPLAY_WORD_ZEHN_1,      DISPLAY_WORD_VOR,  DISPLAY_WORD_NONE }},          // 50
-		{1, 1, {DISPLAY_WORD_FUENF_1,     DISPLAY_WORD_VOR,  DISPLAY_WORD_NONE }},          // 55
+	{                                                                                         // ClockMinutesTable[1][] = OSSI
+		{0},                                                                                  // 00
+		{1, 0, {DISPLAY_WORD_FUENF,       DISPLAY_WORD_NACH, DISPLAY_WORD_NONE   }},          // 05
+		{1, 0, {DISPLAY_WORD_ZEHN,        DISPLAY_WORD_NACH, DISPLAY_WORD_NONE   }},          // 10
+		{1, 1, {DISPLAY_WORD_VIERTEL,     DISPLAY_WORD_NONE, DISPLAY_WORD_NONE   }},          // 45
+		{1, 1, {DISPLAY_WORD_ZEHN,        DISPLAY_WORD_VOR,  DISPLAY_WORD_HALB   }},          // 20
+		{1, 1, {DISPLAY_WORD_FUENF,       DISPLAY_WORD_VOR,  DISPLAY_WORD_HALB   }},          // 25
+		{1, 1, {DISPLAY_WORD_HALB,        DISPLAY_WORD_NONE, DISPLAY_WORD_NONE   }},          // 30
+		{1, 1, {DISPLAY_WORD_FUENF,       DISPLAY_WORD_NACH, DISPLAY_WORD_HALB   }},          // 35
+		{1, 1, {DISPLAY_WORD_ZEHN,        DISPLAY_WORD_NACH, DISPLAY_WORD_HALB   }},          // 40
+		{1, 1, {DISPLAY_WORD_DREIVIERTEL, DISPLAY_WORD_NONE, DISPLAY_WORD_NONE   }},          // 15
+		{1, 1, {DISPLAY_WORD_ZEHN,        DISPLAY_WORD_VOR,  DISPLAY_WORD_NONE   }},          // 50
+		{1, 1, {DISPLAY_WORD_FUENF,       DISPLAY_WORD_VOR,  DISPLAY_WORD_NONE   }},          // 55
 	},
 
-	{                                                                                       // ClockMinutesTable[2][] = RHEIN-RUHR
-		{0},                                                                                // 00
-		{1, 0, {DISPLAY_WORD_FUENF_1,     DISPLAY_WORD_NACH, DISPLAY_WORD_NONE }},          // 05
-		{1, 0, {DISPLAY_WORD_ZEHN_1,      DISPLAY_WORD_NACH, DISPLAY_WORD_NONE }},          // 10
-		{1, 0, {DISPLAY_WORD_VIERTEL,     DISPLAY_WORD_NACH, DISPLAY_WORD_NONE }},          // 15
-		{1, 0, {DISPLAY_WORD_ZWANZIG,     DISPLAY_WORD_NACH, DISPLAY_WORD_NONE }},          // 20
-		{1, 1, {DISPLAY_WORD_FUENF_1,     DISPLAY_WORD_VOR,  DISPLAY_WORD_HALB }},          // 25
-		{1, 1, {DISPLAY_WORD_HALB,        DISPLAY_WORD_NONE, DISPLAY_WORD_NONE }},          // 30
-		{1, 1, {DISPLAY_WORD_FUENF_1,     DISPLAY_WORD_NACH, DISPLAY_WORD_HALB }},          // 35
-		{1, 1, {DISPLAY_WORD_ZWANZIG,     DISPLAY_WORD_VOR,  DISPLAY_WORD_NONE }},          // 40
-		{1, 1, {DISPLAY_WORD_VIERTEL,     DISPLAY_WORD_VOR,  DISPLAY_WORD_NONE }},          // 45
-		{1, 1, {DISPLAY_WORD_ZEHN_1,      DISPLAY_WORD_VOR,  DISPLAY_WORD_NONE }},          // 50
-		{1, 1, {DISPLAY_WORD_FUENF_1,     DISPLAY_WORD_VOR,  DISPLAY_WORD_NONE }},          // 55
+	{                                                                                         // ClockMinutesTable[2][] = RHEIN-RUHR
+		{0},                                                                                  // 00
+		{1, 0, {DISPLAY_WORD_FUENF,       DISPLAY_WORD_NACH, DISPLAY_WORD_NONE   }},          // 05
+		{1, 0, {DISPLAY_WORD_ZEHN,        DISPLAY_WORD_NACH, DISPLAY_WORD_NONE   }},          // 10
+		{1, 0, {DISPLAY_WORD_VIERTEL,     DISPLAY_WORD_NACH, DISPLAY_WORD_NONE   }},          // 15
+		{1, 0, {DISPLAY_WORD_ZWANZIG,     DISPLAY_WORD_NACH, DISPLAY_WORD_NONE   }},          // 20
+		{1, 1, {DISPLAY_WORD_FUENF,       DISPLAY_WORD_VOR,  DISPLAY_WORD_HALB   }},          // 25
+		{1, 1, {DISPLAY_WORD_HALB,        DISPLAY_WORD_NONE, DISPLAY_WORD_NONE   }},          // 30
+		{1, 1, {DISPLAY_WORD_FUENF,       DISPLAY_WORD_NACH, DISPLAY_WORD_HALB   }},          // 35
+		{1, 1, {DISPLAY_WORD_ZWANZIG,     DISPLAY_WORD_VOR,  DISPLAY_WORD_NONE   }},          // 40
+		{1, 1, {DISPLAY_WORD_VIERTEL,     DISPLAY_WORD_VOR,  DISPLAY_WORD_NONE   }},          // 45
+		{1, 1, {DISPLAY_WORD_ZEHN,        DISPLAY_WORD_VOR,  DISPLAY_WORD_NONE   }},          // 50
+		{1, 1, {DISPLAY_WORD_FUENF,       DISPLAY_WORD_VOR,  DISPLAY_WORD_NONE   }},          // 55
 	},
 
-	{                                                                                       // ClockMinutesTable[3][] = SCHWABEN
-		{0},                                                                                // 00
-		{1, 0, {DISPLAY_WORD_FUENF_1,     DISPLAY_WORD_NACH, DISPLAY_WORD_NONE }},          // 05
-		{1, 0, {DISPLAY_WORD_ZEHN_1,      DISPLAY_WORD_NACH, DISPLAY_WORD_NONE }},          // 10
-		{1, 1, {DISPLAY_WORD_VIERTEL,     DISPLAY_WORD_NONE, DISPLAY_WORD_NONE }},          // 45
-		{1, 0, {DISPLAY_WORD_ZWANZIG,     DISPLAY_WORD_NACH, DISPLAY_WORD_NONE }},          // 20
-		{1, 1, {DISPLAY_WORD_FUENF_1,     DISPLAY_WORD_VOR,  DISPLAY_WORD_HALB }},          // 25
-		{1, 1, {DISPLAY_WORD_HALB,        DISPLAY_WORD_NONE, DISPLAY_WORD_NONE }},          // 30
-		{1, 1, {DISPLAY_WORD_FUENF_1,     DISPLAY_WORD_NACH, DISPLAY_WORD_HALB }},          // 35
-		{1, 1, {DISPLAY_WORD_ZWANZIG,     DISPLAY_WORD_VOR,  DISPLAY_WORD_NONE }},          // 40
-		{1, 1, {DISPLAY_WORD_DREIVIERTEL, DISPLAY_WORD_NONE, DISPLAY_WORD_NONE }},          // 15
-		{1, 1, {DISPLAY_WORD_ZEHN_1,      DISPLAY_WORD_VOR,  DISPLAY_WORD_NONE }},          // 50
-		{1, 1, {DISPLAY_WORD_FUENF_1,     DISPLAY_WORD_VOR,  DISPLAY_WORD_NONE }},          // 55
+	{                                                                                         // ClockMinutesTable[3][] = SCHWABEN
+		{0},                                                                                  // 00
+		{1, 0, {DISPLAY_WORD_FUENF,       DISPLAY_WORD_NACH, DISPLAY_WORD_NONE   }},          // 05
+		{1, 0, {DISPLAY_WORD_ZEHN,        DISPLAY_WORD_NACH, DISPLAY_WORD_NONE   }},          // 10
+		{1, 1, {DISPLAY_WORD_VIERTEL,     DISPLAY_WORD_NONE, DISPLAY_WORD_NONE   }},          // 45
+		{1, 0, {DISPLAY_WORD_ZWANZIG,     DISPLAY_WORD_NACH, DISPLAY_WORD_NONE   }},          // 20
+		{1, 1, {DISPLAY_WORD_FUENF,       DISPLAY_WORD_VOR,  DISPLAY_WORD_HALB   }},          // 25
+		{1, 1, {DISPLAY_WORD_HALB,        DISPLAY_WORD_NONE, DISPLAY_WORD_NONE   }},          // 30
+		{1, 1, {DISPLAY_WORD_FUENF,       DISPLAY_WORD_NACH, DISPLAY_WORD_HALB   }},          // 35
+		{1, 1, {DISPLAY_WORD_ZWANZIG,     DISPLAY_WORD_VOR,  DISPLAY_WORD_NONE   }},          // 40
+		{1, 1, {DISPLAY_WORD_DREIVIERTEL, DISPLAY_WORD_NONE, DISPLAY_WORD_NONE   }},          // 15
+		{1, 1, {DISPLAY_WORD_ZEHN,        DISPLAY_WORD_VOR,  DISPLAY_WORD_NONE   }},          // 50
+		{1, 1, {DISPLAY_WORD_FUENF,       DISPLAY_WORD_VOR,  DISPLAY_WORD_NONE   }},          // 55
 	}
 };
 
